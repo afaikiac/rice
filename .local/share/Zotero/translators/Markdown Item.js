53b02ab4d3848229d3f808e8e6fce723986e5305
afaikiac <afaikiac@tuta.io>
2023-02-07 16:32:34 +0300

    [zotero] custom markdown translator

---
+++ .local/share/Zotero/translators/Markdown Item.js
@@ -0,0 +1,34 @@
+{
+"translatorID":"37865636-a6e8-11ed-afa1-0242ac120002",
+"translatorType":2,
+"label":"Markdown Item",
+"creator":"afaikiac, chatgpt",
+"target":"markdown",
+"minVersion":"2.0",
+"maxVersion":"",
+"priority":200,
+"inRepository":false,
+"lastUpdated":"2023-02-07"
+}
+
+function doExport() {
+  var item;
+  while (item = Zotero.nextItem()) {
+		var author = item.creators && item.creators[0] ? item.creators[0].lastName : '';
+    var title = item.title;
+    var year = item.date;
+
+    var markdown = '';
+		if (author && year) {
+			markdown = '[' + author + ', ' + year + ']';
+		} else if (author) {
+			markdown = '[' + author + ']';
+		} else if (title) {
+			markdown = '[' + title + ']';
+		}
+		
+		if (markdown) {
+			Zotero.write('(' + markdown + '(zotero://select/library/items/' + item.key + '))');
+		} else Zotero.write("Unexpected metadata")
+  }
+}
