501813181a7a0f66de460b854e96b36cdd1ae22b
afaikiac <afaikiac@tuta.io>
2023-03-04 04:53:47 +0300

    [fish] update abbr

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,9 +1,5 @@
 if status is-interactive
 
-  abbr -a SI systemctl status
-  abbr -a SS systemctl start
-  abbr -a SE systemctl enable
-  abbr -a SD systemctl disable
-  abbr -a SR systemctl restart
+  abbr -a SS sudo systemctl
 
 end
