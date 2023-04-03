1ee00856096730aad1e23273e5fa5ef1b4f7d44a
afaikiac <afaikiac@tuta.io>
2023-04-04 00:35:16 +0300

    [fish, bin] move abbr to fish

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -2,8 +2,14 @@ if status is-interactive
 
   abbr --position anywhere S  "systemctl"
   abbr SS "sudo systemctl"
+
   abbr pdf-merge "gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=merged.pdf"
+
   abbr docker-image-rmi-none "docker images | tail -n +2 | awk '{if (\$1==\"<none>\") print \$3}' | xargs docker image rmi -f"
   abbr docker-container-rm-all "docker container ls -a | tail -n +2 | awk '{print \$1}' | xargs docker container rm"
+
+  abbr pip-uninstall-all "pip3 freeze | awk -v FS='==' '{print \$1}' | xargs pip3 uninstall --yes"
+
+  abbr pyenvx-install "FILE=\"~/.local/bin/pyenvx\" bash -c \"curl -fsSl https://raw.githubusercontent.com/afaikiac/pyenvx/main/pyenvx.bash -o \$FILE && chmod +x \$FILE && echo 'pyenvx was installed!'\""
 
 end
